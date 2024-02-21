960cc7f59d8afbddbc9dc2a4ae94e620ff8ce641
Kingcedru <byishimocedrick@gmail.com>
2024-02-21 18:32:30 +0200

    creating a post route

--- app.js
+++ app.js
@@ -2,12 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://byishimocedrick:node_exercise@cluster0.cnsuu9p.mongodb.net/"
-);
+mongoose
+  .connect(
+    "mongodb+srv://byishimocedrick:node_exercise@cluster0.cnsuu9p.mongodb.net/"
+  )
+  .then(() => console.log("connected to database"))
+  .catch(console.error());
+
+const Items = require("./models/items");
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get("/items", async (req, res) => {
+  const items = await Items.find();
+  res.json(items);
+});
+
+app.post("/items", (req, res) => {
+  const item = new Items({
+    id: req.body.id,
+    name: req.body.name,
+  });
+  item.save();
+  res.json(item);
+});
 
 app.listen(3000, () => {
   console.log("listening");
--- models/items.js
+++ models/items.js
@@ -1,13 +1,10 @@
-const { timeStamp } = require("console");
 const mongoose = require("mongoose");
-const { type } = require("os");
 const schema = mongoose.Schema;
 
-const ItemsSchema = new Schema({
-  id: { type: Number, required: true },
+const ItemsSchema = new schema({
   name: { type: String, required: true },
   timeStamp: { type: String, default: Date.now() },
 });
 
-const Items = mongoose.model("todo", ItemsSchema);
-module.exports = Todo;
+const Items = mongoose.model("Items", ItemsSchema);
+module.exports = Items;
