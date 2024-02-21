e953c1f8ae1010ce495c0c0981647b1962a67126
Kingcedru <byishimocedrick@gmail.com>
2024-02-21 17:25:04 +0200

    adding schema

--- models/items.js
+++ models/items.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const { type } = require("os");
 const schema = mongoose.Schema;
 
-const ItemSchema = new Schema({
+const ItemsSchema = new Schema({
   id: { type: Number, required: true },
   name: { type: String, required: true },
   timeStamp: { type: String, default: Date.now() },
 });
+
+const Items = mongoose.model("todo", ItemsSchema);
+module.exports = Todo;
